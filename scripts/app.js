3b3e7b0ed4b09d25675785f156fa70aca5591c60
Yokie99 <kyleym@uci.edu>
2024-01-24 02:17:42 -0800

    added links to the highlights page

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+let video = document.getElementById("video");
+
+let clip1 = document.getElementById("clip1");
+let clip2 = document.getElementById("clip2");
+let clip3 = document.getElementById("clip3");
+let clip4 = document.getElementById("clip4");
+let clip5 = document.getElementById("clip5");
+let clip6 = document.getElementById("clip6");
+let clip7 = document.getElementById("clip7");
+let clip8 = document.getElementById("clip8");
+
+
+clip1.addEventListener("click", () =>{
+    video.src = ("https://www.youtube.com/embed/kuSMEVhPvTY?si=URs2vLAyUoHbJU7U");
+})
+clip2.addEventListener("click", () =>{
+    video.src = ("https://www.youtube.com/embed/pMZQ_2-qoYI?si=-5N_RqH1irsXKil4");
+})
+clip3.addEventListener("click", () =>{
+    video.src = ("https://www.youtube.com/embed/yVp-jBz-cgA?si=mjOrouzR1NsCWD01");
+})
+clip4.addEventListener("click", () =>{
+    video.src = ("https://www.youtube.com/embed/BvqxSR8D4Pk?si=kpYh0uOeBrcbDqOD");
+})
+clip5.addEventListener("click", () =>{
+    video.src = ("https://www.youtube.com/embed/ZJoxR3yXTRc?si=gJKY-slohqraXH2D");
+})
+clip6.addEventListener("click", () =>{
+    video.src = ("https://www.youtube.com/embed/caCGpPFrGBo?si=EJHu3a00YAmof5Jf");
+})
+clip7.addEventListener("click", () =>{
+    video.src = ("https://www.youtube.com/embed/mfnkpB7XMN4?si=I405wRUNlQkZtTJ3");
+})
+clip8.addEventListener("click", () =>{
+    video.src = ("https://www.youtube.com/embed/6pPKV8lH9qI?si=N59PzUtP9u74Ethz");
+})
